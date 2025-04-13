c3076a4ea8bcdfa5e54613e0dfc4e8c3ccc25f28
Max007001002 <maxxx007001002@gmail.com>
2025-04-13 12:17:39 +0300

    #

--- dags/max_dag_les_9.py
+++ dags/max_dag_les_9.py
@@ -1,15 +1,15 @@
 from airflow import DAG
-from airflow.operators.pyhton import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.hooks.base import BaseHook
 
 from datetime import datetime
 
 # Параметры по умолчанию (константы)
-DEFAULT_DAGS = {
+DEFAULT_ARGS = {
     'owner': 'admin', # Владелец DAG
     'retries': 2,
-    'retry_dalay': 600,
+    'retry_delay': 600,
     'start_date': datetime(2025, 4, 12)
 }
 
@@ -33,7 +33,7 @@ def upload_data(**context):
         dbname='etl',
         sslmode='disable',
         user=connection.login,
-        password=connection.user,
+        password=connection.password,
         host=connection.host,
         port=connection.port,
         connection_timeout=600,
@@ -48,19 +48,19 @@ def upload_data(**context):
     
     writer_wrapper = codecs.getwriter('utf-8') # Кодировка
 
-# Создаем объект writer для записи данных в файл
+    # Создаем объект writer для записи данных в файл
     writer = csv.writer(
         writer_wrapper(file), # Объект writer для записи данных в файл
-        delimeter='\t', # Разделитель
-        linetrminator='\n', # Разделитель
-        qoutechar='"', # Кавычки
-        qouting=csv.QUOTE_MINIMAL # Кавычки
+        delimiter='\t', # Разделитель
+        lineterminator='\n', # Разделитель строк
+        quotechar='"', # Кавычки
+        quoting=csv.QUOTE_MINIMAL # Режим кавычек
     )
 
     writer.writerows(data) # Записываем данные в файл
     file.seek(0) # Смещаем курсор в начало файла. Если мы этого не сделаем, то ничего не запишется в файл
 
-# До этого момента мы создали объект BytesIO, который содержит в себе данные из базы данных. Теперь нам нужно записать эти данные в файл и загрузить его в S3. До этого все хранится в нашей ОС.
+    # До этого момента мы создали объект BytesIO, который содержит в себе данные из базы данных. Теперь нам нужно записать эти данные в файл и загрузить его в S3. До этого все хранится в нашей ОС.
 
     connection = BaseHook.get_connection('conn_s3') # Получаем соединение с S3
     # Создаем клиента для работы с S3
@@ -81,25 +81,25 @@ def upload_data(**context):
 
 
 def combine_data(**context):
-    import psycopg2
+    import psycopg2 as pg
 
     sql_query = f"""
         INSERT INTO admin_agg_table
         SELECT 
             lti_user_id,
             attempt_type,
-            COUNT(1), # Количество попыток (всех)
-            COUNT(CASE WHEN is_correct THEN NULL ELSE 1 END) AS attempt_fails_count, # Количество попыток (неправильных)
+            COUNT(1), -- Количество попыток (всех)
+            COUNT(CASE WHEN is_correct THEN NULL ELSE 1 END) AS attempt_fails_count, -- Количество попыток (неправильных)
             '{context['ds']}'::timestamp
         FROM admin_agg_table
         WHERE created_at >= '{context['ds']}'::timestamp
-            AND created_at < '{context['ds']}'::timestamp + INTERVAL '1 days' # Данным интервалом запроса мы берем 1 день. Но чтобы получить данные за 1 день, можно было бы сделать приравнивание, НО не стоит этого делать, потому что процесс очень сильно замедлится (особенно на продакшине), и так же не стоит писать по вермени до 23:59:59, так как запись производиться может в долях секунды, и мы что-то потеряем.
+            AND created_at < '{context['ds']}'::timestamp + INTERVAL '1 days' -- Данным интервалом запроса мы берем 1 день. Но чтобы получить данные за 1 день, можно было бы сделать приравнивание, НО не стоит этого делать, потому что процесс очень сильно замедлится (особенно на продакшине), и так же не стоит писать по вермени до 23:59:59, так как запись производиться может в долях секунды, и мы что-то потеряем.
         GROUP BY 1, 2;
     """
 
     connection = BaseHook.get_connection('conn_pg') # Получаем соединение с базой данных (эти данные находятся в Airflow во вкладке Connections)
-# Создаем соединение с базой данных
-    with pg,connect(
+    # Создаем соединение с базой данных
+    with pg.connect(
         dbname='etl', # Название базы данных
         sslmode='disable', # Режим шифрования
         user=connection.login, # Логин
@@ -110,7 +110,7 @@ def combine_data(**context):
         keepalives=600, # Таймаут соединения
         tcp_user_timeout=600 # Таймаут соединения
     ) as conn:
-        cursor-conn.cursor() # Создаем курсор
+        cursor = conn.cursor() # Создаем курсор
         cursor.execute(sql_query) # Выполняем запрос
         conn.commit() # Сохраняем изменения
     
@@ -118,7 +118,7 @@ def combine_data(**context):
 with DAG(
     dag_id='max_dag_les_9',
     tags=['max_khalilov', '7'],
-    shedule='@daily',
+    schedule='@daily',
     default_args=DEFAULT_ARGS, # Параметры по умолчанию
     max_active_runs=1,
     max_active_tasks=1
@@ -132,7 +132,7 @@ with DAG(
         python_callable=combine_data,
     )
 
-    upload_data =  PythonOperator( # Загрузка данных в S3
+    upload_data = PythonOperator( # Загрузка данных в S3
         task_id='upload_data',
         python_callable=upload_data,
     )  
