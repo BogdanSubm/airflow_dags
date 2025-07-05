7825de9a2bcec4d04ee0b7a82907df40b4a4c339
eduardosick <eduardo_sick@outlook.com>
2025-07-05 14:22:27 +0300

    load_api_pg_s3_ed.ver1.19

--- dags/operators/branch_operator_ed.py
+++ dags/operators/branch_operator_ed.py
@@ -1,16 +1,22 @@
 from airflow.models import BaseOperator
-
+from airflow.utils.context import Context
 import pendulum
 
 class MyBranchOperator(BaseOperator):
+    """Кастомный оператор ветвления с правильной логикой пропуска"""
     
     def __init__(self, num_days, **kwargs):
         super().__init__(**kwargs)
         self.num_days = num_days
         
-    def execute(self, context):
-        dt = pendulum.parse(context['ds'])
-
+    def execute(self, context: Context):
+        execution_date = context['ds']
+        dt = pendulum.parse(execution_date)
+        self.log.info(f"Checking day {dt.day} against {self.num_days}")
+        
         if dt.day in self.num_days:
+            self.log.info("Returning 'agg_data' as next task")
             return 'agg_data'
-        return 'upload_data'
+        else:
+            self.log.info(f"Day {dt.day} not in {self.num_days}, skipping 'agg_data'")
+            return 'upload_data'
