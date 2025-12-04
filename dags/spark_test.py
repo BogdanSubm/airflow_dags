09ddbf027b814eeaac50ebd4d74dd31897571988
HowBoringAndSmall <bogdan.vp2015@yandex.ru>
2025-12-04 10:28:12 +0300

    Тестим spark

--- dags/spark_test.py
+++ dags/spark_test.py
@@ -1,7 +1,11 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("spark-pi").getOrCreate()
+
 data = spark.range(0, 1000000)
-pi = data.rdd.map(lambda x: 4.0 / (1 + (x % 1000) ** 2)).mean()
+
+pi = data.rdd.map(lambda row: 4.0 / (1 + (row.id % 1000) ** 2)).mean()
+
 print(f"Approx PI = {pi}")
+
 spark.stop()
