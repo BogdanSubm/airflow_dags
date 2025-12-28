ad5b03ff75b766727fff5c4d2934b82e82af8b29
mishandri <mishandri@gmail.com>
2025-12-28 16:30:30 +0300

    lesson 15, sqlsensor + dag

---
+++ dags/mikhail_k/mikhail_k_lesson_15_sensor.py
@@ -0,0 +1,38 @@
+import psycopg2 as pg
+from airflow.hooks.base import BaseHook
+from airflow.sensors.base import BaseSensorOperator
+from airflow.utils.decorators import apply_defaults
+
+class SqlSensor(BaseSensorOperator):
+    template_fields = ('sql', ) # данный параметр прописывается, чтобы можно было использовать переменные в sql запросе, как template jinja
+
+    def __init__(self, sql: str, **kwargs):
+        super().__init__(**kwargs)
+        self.sql = sql
+
+    def poke(self, context) -> bool: # данный метод проверяет, выполнился ли запрос. Он идет вместо execute
+        connection = BaseHook.get_connection('conn_pg')
+
+        with pg.connect(
+            dbname='etl',
+            sslmode='disable',
+            user=connection.login,
+            password=connection.password,
+            host=connection.host,
+            port=connection.port,
+            connect_timeout=600,
+            keepalives_idle=600,
+            tcp_user_timeout=600
+        ) as conn:
+            cursor = conn.cursor() # Создаем курсор 
+
+            self.log.info(self.sql) # Логирование sql запроса
+
+            cursor.execute(self.sql) # Выполняем запрос
+            result = cursor.fetchone() # Получаем данные. fetchone - получает одну строку
+
+        if result[0] > 0:
+            return True
+        else:
+            return False
+            
