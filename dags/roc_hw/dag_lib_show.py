5457a2817f98188cf6faa432f5bef367dcf18b63
Andres <ambidexteron@gmail.com>
2026-03-24 15:42:22 +0300

    roc_hw_fin

--- dags/roc_hw/dag_lib_show.py
+++ dags/roc_hw/dag_lib_show.py
@@ -4,17 +4,17 @@ from airflow.operators.empty import EmptyOperator
 from airflow.operators.bash import BashOperator
 
 DEFAULT_ARGS = {
-    'owner': 'rocknmove',
+    'owner': 'roc',
     'retries': 2,
     'retries_delay': 600,
     'start_date': datetime(2026, 2, 16)
 }
 
-with DAG(dag_id='rocknmove_lib_show',
-         schedule='03**1',
+with DAG(dag_id='roc_lib_show',
+         schedule='@once',
          start_date=datetime.today(),
          default_args=DEFAULT_ARGS,
-         tags=['rocknmove']) as dag:
+         tags=['roc']) as dag:
 
     start_dag = EmptyOperator(task_id='start_dag')
     end_dag = EmptyOperator(task_id='end_dag')
--- dags/roc_hw/plugins/sqls.py
+++ dags/roc_hw/plugins/sqls.py
@@ -2,7 +2,7 @@
 
 sql_creates_dict = {
     'rocknmove_raw_data': """
-        CREATE TABLE IF NOT EXISTS rocknmove_raw_data (
+        CREATE TABLE IF NOT EXISTS {} (
             id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
             , lti_user_id TEXT NOT NULL
             , oauth_consumer_key TEXT
@@ -12,19 +12,19 @@ sql_creates_dict = {
             , attempt_type TEXT CHECK (attempt_type in ('run', 'submit'))
             , created_at TIMESTAMP
             , date_tag TEXT
-            , CONSTRAINT unique_check UNIQUE (lti_user_id, created_at)
+            , UNIQUE (lti_user_id, created_at)
             ) """,
 
     'rocknmove_users': """
-        CREATE TABLE IF NOT EXISTS rocknmove_users (
+        CREATE TABLE IF NOT EXISTS {} (
             id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
             , lti_user_id TEXT NOT NULL
             , first_activity TIMESTAMP NOT NULL
-            , CONSTRAINT unique_check_2 UNIQUE (lti_user_id)
+            , UNIQUE (lti_user_id)
             ) """,
 
     'rocknmove_data_agg1': """
-        CREATE TABLE IF NOT EXISTS rocknmove_data_agg1 (
+        CREATE TABLE IF NOT EXISTS {} (
             id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
             , period_start DATE
             , period_end DATE
@@ -33,14 +33,14 @@ sql_creates_dict = {
             , unique_users INTEGER
             , new_users INTEGER
             , date_tag TEXT
-            , CONSTRAINT unique_check_3 UNIQUE (period_start, period_end)
+            , UNIQUE (period_start, period_end)
             )"""
 }
 
 
 # ================= DML and DQL ===================
 sql_agg = """
-    INSERT INTO rocknmove_data_agg1 
+    INSERT INTO roc_data_agg1 
     (period_start, period_end, attempts_total, correct_attempts, unique_users, new_users, date_tag) 
         SELECT
             %s::date AS period_start
@@ -50,25 +50,25 @@ sql_agg = """
             , count(DISTINCT d.lti_user_id) AS unique_users
             , count(DISTINCT CASE WHEN u.lti_user_id IS NULL THEN d.lti_user_id ELSE NULL END) AS new_users
             , date_tag
-        FROM rocknmove_raw_data d
-        LEFT JOIN (SELECT DISTINCT lti_user_id FROM rocknmove_raw_data WHERE created_at < %s::date) u ON d.lti_user_id=u.lti_user_id
+        FROM roc_raw_data d
+        LEFT JOIN (SELECT DISTINCT lti_user_id FROM roc_raw_data WHERE created_at < %s::date) u ON d.lti_user_id=u.lti_user_id
         WHERE created_at >= %s::date
             AND created_at < %s::date
-        GROUP BY date_tag
-    ON CONFLICT ON CONSTRAINT unique_check_3 DO NOTHING
+        GROUP BY date_tag, period_start, period_end
+    ON CONFLICT (period_start, period_end) DO NOTHING
 """
 
 sql_add_users = """
-    INSERT INTO rocknmove_users 
+    INSERT INTO roc_users 
     (lti_user_id, first_activity) 
         SELECT DISTINCT 
             lti_user_id
             , min(created_at) AS first_activity
-        FROM rocknmove_raw_data
+        FROM roc_raw_data
         WHERE created_at >= %s::date
             AND created_at < %s::date
         GROUP BY lti_user_id
-    ON CONFLICT ON CONSTRAINT unique_check_2 DO NOTHING
+    ON CONFLICT (lti_user_id) DO NOTHING
 """
 sql_check_table = """
     SELECT 1
