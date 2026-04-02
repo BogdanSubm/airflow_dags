d377830c4c60d147ce1acf50404126473427bfbc
bashmachello <ipaspiridonov@yandex.ru>
2026-04-02 10:51:30 +0300

    update dag file

--- dags/spiridonov/config/agg_config.py
+++ dags/spiridonov/config/agg_config.py
@@ -1,6 +1,6 @@
 config = [
     {
-        'table_name': 'roc_test_agg1',
+        'table_name': 'roc_test_agg1_spiridonov',
         'table_ddl': 'create table if not exists {} (category text, amount int, load_date date)',
         'table_dml': '''
             insert into {} 
@@ -11,7 +11,7 @@ config = [
         'need_to_export': False,
     },
     {
-        'table_name': 'roc_test_agg2',
+        'table_name': 'roc_test_agg2_spiridonov',
         'table_ddl': 'create table if not exists {} (category text, amount int, load_date date)',
         'table_dml': '''
             insert into {} 
