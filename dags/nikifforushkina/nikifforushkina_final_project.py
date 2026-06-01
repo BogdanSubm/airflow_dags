7fcc86908d6574ab83a1b5b7130189d29aa456e6
Александра <nikifforushkina@gmail.com>
2026-06-01 22:05:13 +0300

    Добавлены правки

--- dags/nikifforushkina/nikifforushkina_final_project.py
+++ dags/nikifforushkina/nikifforushkina_final_project.py
@@ -5,7 +5,7 @@ from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow.hooks.base import BaseHook
 
-from dags.nikifforushkina_PostgresOperator import PostgresOperator
+from nikifforushkina_PostgresOperator import PostgresOperator
 
 DEFAULT_ARGS = {
     'owner': 'nikifforushkina',
