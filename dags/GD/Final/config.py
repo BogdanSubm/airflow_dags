204ef689d09950c2252013fde44e6222352546d4
Danil026 <d.gilyazov26@gmail.com>
2026-06-27 18:53:16 +0300

    Fix sql in confiq

--- dags/GD/Final/config.py
+++ dags/GD/Final/config.py
@@ -16,26 +16,24 @@ config = [
             GROUP BY lti_user_id
         """,
         "need_to_export": True,
-        # Явно указываем колонки для CSV. Если нужны все - поставь ["*"]
         "export_columns": ["lti_user_id", "total_rows"],
     },
     {
         "table_name": "test_gd_agg2",
         "table_ddl": """
             CREATE TABLE IF NOT EXISTS public.test_gd_agg2 (
-                lti_user_id varchar(1000),
+                lis_result_sourcedid varchar(1000),
                 total_rows bigint
             )
         """,
         "table_dml": """
             SELECT
-                lti_user_id,
+                lis_result_sourcedid,
                 COUNT(*) AS total_rows
             FROM public.test_gd
             GROUP BY lis_result_sourcedid
         """,
         "need_to_export": True,
-        # Явно указываем колонки для CSV. Если нужны все - поставь ["*"]
         "export_columns": ["lis_result_sourcedid", "total_rows"],
     }
     
